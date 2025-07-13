86ae5eae2a1fd4706b4f2932f2b26059dee4b018
Mohd Azeem <168483609+17azeem@users.noreply.github.com>
2025-07-13 06:27:34 +0000

    data types

---
+++ .devcontainer/01_basics/01_dataTypes.js
@@ -0,0 +1,15 @@
+//data types
+let name = "azeem"
+let age=18
+let isLoggedin=false
+
+console.log(typeof "azeem")
+//number => 2 to power 53
+//bigint
+//string => "" or ''
+//boolean=> true/false
+//null => standalone value
+// undefined
+//symbol => unique
+
+//object
