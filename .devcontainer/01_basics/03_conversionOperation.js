a267788e5362b94179bdd1548f6ca66bbf85ce2a
Mohd Azeem <168483609+17azeem@users.noreply.github.com>
2025-07-13 06:46:23 +0000

    conversion of dataTypes

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score="33"
+console.log(score)
+console.log(typeof(score))
+let valueInNumber = Number(score)
+console.log(valueInNumber)
+console.log(typeof(valueInNumber))
+
+//"33" => 33
+/*
+"33abc" => NaN
+true=>1 false=>0
+
+*/
+//boolean conversion
+/*
+1=> true; 0=>false
+""=> false
+"azeem"=>true
+*/
+
+//string conversion
+let n=33
+let string=String(n)
+console.log(string)
+console.log(typeof(string))
