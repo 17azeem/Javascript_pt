143a743eb030336ac7866ce8250068bd5476fe7f
Mohd Azeem <168483609+17azeem@users.noreply.github.com>
2025-07-13 07:23:21 +0000

    comparision of data types

---
+++ .devcontainer/01_basics/04_comparision.js
@@ -0,0 +1,16 @@
+//comparision ==, !=, < ,> , <=, >=
+
+//not recommended to compare different data types
+console.log("2">0)
+console.log(2>"0")
+
+// console.log(null==0)
+// console.log(null>0)
+// console.log(null>=0)
+// console.log("undefined comparision")
+// console.log(undefined==0)
+// console.log(undefined>0)
+// console.log(undefined<0)
+
+// === check with data type
+console.log("2"===2)
