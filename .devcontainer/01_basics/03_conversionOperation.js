d8ded3f99bb1d442042161745fac1a0b377c53d8
Mohd Azeem <168483609+17azeem@users.noreply.github.com>
2025-07-13 07:09:29 +0000

    Operations and type conversion (when string is added to the number or viceversa)

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -23,3 +23,23 @@ let n=33
 let string=String(n)
 console.log(string)
 console.log(typeof(string))
+
+//**********operations */
+//arithmetic operations
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**3)
+console.log(2/2)//quotient
+console.log(2%2)//remainder
+
+//string operations
+let str1="hello"
+let str2=" azeem"
+console.log(str1+str2)
+
+//the presence of a string in an addition operation with a number (or other primitive) will always result in string concatenation, as the non-string operand is coerced to a string. For other arithmetic operations, strings are coerced to numbers.
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+2+"2")
