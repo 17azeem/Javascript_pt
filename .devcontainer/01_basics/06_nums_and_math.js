575df32fd3f70cb61f1944247912b4299056d48b
Mohd Azeem <168483609+17azeem@users.noreply.github.com>
2025-07-14 14:24:10 +0000

    Math operations

--- .devcontainer/01_basics/06_nums_and_math.js
+++ .devcontainer/01_basics/06_nums_and_math.js
@@ -18,3 +18,31 @@ const hundreds=1000000
 console.log(hundreds.toLocaleString())//international number system
 console.log(hundreds.toLocaleString('en-IN'))
 //Indian number system
+
+/*++++++++++++math++++++++++++++ */
+console.log(Math);//run in console to see the properties
+//Returns the absolute value of a number
+console.log(Math.abs(-4))// o/p : 4
+//Returns a supplied numeric expression rounded to the nearest integer.
+console.log(Math.round(4.6)) // op: 5
+//Returns the smallest integer greater than or equal to its numeric argument.
+console.log("ceil")
+console.log(Math.ceil(4.2))//5
+
+//Returns the greatest integer less than or equal to its numeric argument.
+console.log("floor")
+console.log(Math.floor(4.9))//4
+
+console.log(Math.min(4,3,6,8))//min : 3
+console.log(Math.max(4,3,6,8))// max : 8
+
+//generating random values
+console.log(Math.random()); // generate values b/w 0 and 1
+console.log((Math.random()*10)+1);//start from 1
+console.log(Math.floor(Math.random()*10)+1)//remove decimal value
+
+//formula for generating random no's in range
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
