b2cb3ad248ac5004bbbe1451af2fccf812179798
Mohd Azeem <168483609+17azeem@users.noreply.github.com>
2025-07-14 14:33:21 +0000

    change file name

---
+++ .devcontainer/01_basics/02_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "azeem@gmail.com"
+var accountPassword="12345"
+//not recommended to use with keyword
+
+accountCity="Hyd"
+
+//single line comment
+// accountId=2 not allowed 
+
+accountEmail= "az@google.com"
+accountPassword="232323"
+accountCity="telangana"
+
+console.log(accountId)
+console.table([accountEmail,accountPassword,accountCity])
